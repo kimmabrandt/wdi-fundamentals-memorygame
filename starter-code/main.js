dafc63f7d2acf69a88ecd778c88dee035cce81ff
Kim Brandt <kimbrandt@Kims-MacBook-Pro.local>
2016-07-14 21:38:03 -0700

    memory game match alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,14 @@
-console.log("JS file is connected to HTML! Woo!")
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+if (cardTwo === cardFour) {
+window.alert("Sorry, try again.");
+}
+else if (cardOne === cardTwo) {
+	window.alert("You found a match!");
+}
+else if (cardThree === cardFour) {
+	window.alert("You found a match!")
+}
